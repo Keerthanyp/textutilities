0cbe72e42c45a6be7a18abfb6207c499fbf28a58
keerthangowdayp <keerthan.gowda@intimasolutions.com>
2022-12-15 15:58:28 +0530

    naming modules

---
+++ module1.mjs
@@ -0,0 +1,11 @@
+// import ui from "./module2.mjs";
+
+// console.log(ui);
+// import dza from "./module2.mjs";
+//import anything and it will return the default export part from the component.
+// console.log(dza);
+
+import {a,b,c} from "./module2.mjs";
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a='suhan';
+const b='keert';
+const c='su';
+
+export default b;
+
+export {a}; //this type of export is done if default export is not working
+export {b};
+export {c};
