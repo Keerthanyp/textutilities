b92d23c20f864ea58542f4c5b2dd204be8e8e635
keerthangowdayp <keerthan.gowda@intimasolutions.com>
2022-12-23 12:08:06 +0530

    Updates

---
+++ precache-manifest.32f8560b84415596acb51aee9004adb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7560ed8fd832e295ceef",
+    "url": "/textutilities/static/css/main.4559f75d.chunk.css"
+  },
+  {
+    "revision": "7560ed8fd832e295ceef",
+    "url": "/textutilities/static/js/main.7560ed8f.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutilities/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutilities/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "98e3e35122a40f49e324",
+    "url": "/textutilities/static/js/runtime~main.98e3e351.js"
+  },
+  {
+    "revision": "fc90ff48533827fb08adc49cd208d806",
+    "url": "/textutilities/index.html"
+  }
+];
